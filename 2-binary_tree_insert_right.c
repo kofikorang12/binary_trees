ff078a4e8162b930424bdce872567d03a504434c
Kofi korang <jbaah69@gmail.com>
2023-10-30 09:59:33 +0000

    binary tress

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts new node to the right-child of another parent
+ * @parent: parent of new node
+ * @value: value of new node
+ *
+ * Return: pointer to new node, NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+
+	return (new_node);
+}
